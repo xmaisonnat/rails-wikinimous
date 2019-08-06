8f6f2cda9fb1b71014312a70b8eaead377378a35
Xavier Maisonnat <x.maisonnat@gmail.com>
2019-08-06 16:50:56 +0200

    create seeds and the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'articles', to: 'articles#index', as: :articles
+  get 'articles/new', to: 'articles#new', as: :new_article
+  get 'articles/:id', to: 'articles#show', as: :article
+  # CREATE
+  post 'articles', to: 'articles#create'
+  # UPDATE
+  get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+  patch 'articles/:id', to: 'articles#update'
+  # DELETE
+  delete 'articles/:id', to: 'articles#destroy'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  title = Faker::Lorem.word
+  content = Faker::Lorem.paragraphs
+  new_article = Article.new(title: title, content: content)
+  new_article.save
+end
